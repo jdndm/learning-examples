9340dfb69c216d271690bfc8ae71d87127059ba5
Jack McQuinn <Jack.McQuinn@unisuper.com.au>
2018-08-24 11:33:57 +1000

    Added oop examples

---
+++ part2/examples/oop/oopconcepts.py
@@ -0,0 +1,32 @@
+class Classroom:
+
+    def __init__(self):
+        self._people = []
+
+    def add_person(self, person):
+        self._people.append(person)
+
+    def remove_person(self, person):
+        self._people.remove(person)
+
+    def greet(self):
+        for person in self._people:
+            person.say_hello()
+
+class Person:
+
+    def __init__(self, name):
+        self.name = name
+
+    def say_hello(self):
+        print(id(self))
+        print("Hello, ", self.name)
+
+
+room = Classroom ()
+room.add_person(Person("Jack"))
+room.add_person(Person("Alex"))
+room.add_person(Person("Daniel"))
+
+room.greet()
+
