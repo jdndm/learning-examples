010b191a932bfaf28627d717f9664af8b7a64ec6
Jack McQuinn <Jack.McQuinn@unisuper.com.au>
2018-09-03 16:04:05 +1000

    C# Fundies Module 6

--- Grades/Grades/Program.cs
+++ Grades/Grades/Program.cs
@@ -11,14 +11,36 @@ namespace Grades
         static void Main(string[] args)
         {          
             GradeBook book = new GradeBook();
+
+            book.NameChanged += OnNameChanged;
+
+            book.Name = "Jack's Grade Book";
+            book.Name = "Grade Book";
+
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
-
+          
             GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(stats.AverageGrade);
-            Console.WriteLine(stats.HighestGrade);
-            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(book.Name);
+            WriteResult("Average", stats.AverageGrade);
+            //TODO: Refactor to use only one WriteResult method.
+            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
+        }
+
+        static void OnNameChanged(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
+        }
+
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine($"{description}: {result:F2}");
         }
     }
 }
